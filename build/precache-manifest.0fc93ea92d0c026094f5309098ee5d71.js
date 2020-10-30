1dec986c07fe28e85c0ce00a5a2a0b37df6a0a34
Spendil <spendil@mail.ru>
2020-10-30 15:29:57 +0300

    build

---
+++ build/precache-manifest.0fc93ea92d0c026094f5309098ee5d71.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eaa85f7faffcf5c7652aa6faeb23a01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53b2f2861bbf00855658",
+    "url": "/static/css/2.b5f4ab2d.chunk.css"
+  },
+  {
+    "revision": "aee69624310ff6628ee9",
+    "url": "/static/css/main.c7a534ff.chunk.css"
+  },
+  {
+    "revision": "53b2f2861bbf00855658",
+    "url": "/static/js/2.b59bbcfd.chunk.js"
+  },
+  {
+    "revision": "53ad072508a90a9257443e204b5341d9",
+    "url": "/static/js/2.b59bbcfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aee69624310ff6628ee9",
+    "url": "/static/js/main.0216c58d.chunk.js"
+  },
+  {
+    "revision": "89246c20e4a66bdd6088",
+    "url": "/static/js/runtime-main.28ba42de.js"
+  },
+  {
+    "revision": "b137d64f8e6129f209b23463f6e7faf6",
+    "url": "/static/media/bitquery_logo.b137d64f.png"
+  }
+]);
