2e9939508dba331135747bce7466da454d25cbf1
Spendil <spendil@mail.ru>
2021-01-14 11:21:42 +0300

    build upd

---
+++ build/precache-manifest.230265499bf19874ff23086cf218564b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34de0b789e3889b6dd4002a3177b9f5d",
+    "url": "https://graphql.bitquery.io/test/index.html"
+  },
+  {
+    "revision": "1bd9ee6d2490ffa1ea65",
+    "url": "https://graphql.bitquery.io/test/static/css/main.css"
+  },
+  {
+    "revision": "1bd9ee6d2490ffa1ea65",
+    "url": "https://graphql.bitquery.io/test/static/js/main.js"
+  },
+  {
+    "revision": "b91ede3f6554395a3b5da6648c6c0df1",
+    "url": "https://graphql.bitquery.io/test/static/js/main.js.LICENSE.txt"
+  },
+  {
+    "revision": "35cf9adfc6ba2132d10524b85524cbc5",
+    "url": "https://graphql.bitquery.io/test/static/media/play.35cf9adf.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.32a017d75c5bec796b9bbdd22e75f773.js"
+  "https://graphql.bitquery.io/test/precache-manifest.230265499bf19874ff23086cf218564b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://graphql.bitquery.io/test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
