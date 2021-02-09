61064fea4ea7add06a3e5ffbbc9ac35e3abdfaf9
Spendil <spendil@mail.ru>
2021-02-09 17:47:59 +0300

    chart version upd, memo onRun

--- src/components/EditorInstance.js
+++ src/components/EditorInstance.js
@@ -150,9 +150,9 @@ const EditorInstance = observer(function EditorInstance({number})  {
 		} catch (e) {}
 		return typesMap
 	}
-	const getResult = async () => {
+	const getResult = useCallback (() => {
 		setLoading(true)
-		const data = await fetcher({query: query[index].query, variables: query[index].variables})
+		fetcher({query: query[index].query, variables: query[index].variables}).then(data =>
 		data.json().then(json => {
 			setDataSource({
 				execute: getResult,
@@ -164,14 +164,14 @@ const EditorInstance = observer(function EditorInstance({number})  {
 				variables: toJS(query[index].variables)
 			})
 			if (!('data' in json)) updateQuery({widget_id: 'json.widget'}, index)
-		})
+			setLoading(false)
+		}))
 		let queryType = getQueryTypes(query[index].query)
 		if (JSON.stringify(queryType) !== JSON.stringify(queryTypes)) {
 			setQueryTypes(queryType)
 		}
-		setLoading(false)
 		setAccordance(true)
-	}
+	}, [index, JSON.stringify(currentQuery)])
 	useEffect(() => {
 		(!dataSource.values && 
 		currentQuery.query &&
