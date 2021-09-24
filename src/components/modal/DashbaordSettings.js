acdfb5566bd7c477c27e95c0f2049ff455e876ea
Spendil <spendil@mail.ru>
2021-09-24 04:43:12 +0300

    db javascript migration added

--- src/components/modal/DashbaordSettings.js
+++ src/components/modal/DashbaordSettings.js
@@ -26,8 +26,8 @@ const DashbaordSettings = observer(function DashbaordSettings({active}) {
 			<div className="form-group flex flex-col align-items-center w-100">
 				<label htmlFor="prejs">Preprocessing</label>
 				<textarea className="query__save mb-3" id="prejs" rows="5" value={prejs} onChange={(e) => setPreJS(e.target.value)}></textarea>
-				<label htmlFor="postjs">Postprocessing</label>
-				<textarea className="query__save mb-3" id="postjs" rows="5" value={postjs} onChange={(e) => setPostJS(e.target.value)}></textarea>
+				{/* <label htmlFor="postjs">Postprocessing</label>
+				<textarea className="query__save mb-3" id="postjs" rows="5" value={postjs} onChange={(e) => setPostJS(e.target.value)}></textarea> */}
 				<button className="button button_filled m-0" onClick={saveSettings}>Done</button>
 			</div>
 			<i className="handler handler__close fas fa-times" onClick={closeHandler} />
