e0c7e9ceb4b49e374eea9ff333cb825779061bdb
Spendil <spendil@mail.ru>
2021-12-14 09:47:59 +0300

    download csv table

--- src/components/bitqueditor/components/WidgetView.js
+++ src/components/bitqueditor/components/WidgetView.js
@@ -1,11 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import CsvIcon from '../../icons/CsvIcon'
 
 function WidgetView({ el, config, dataSource, displayedData, children, renderFunc }) {
-	useEffect(() => {
+	const [table, setTable] = useState(null)
+	const downloadCSV = () => {
+		table.download('csv', 'data.csv')
+	}
+	useEffect(async () => {
 		if (dataSource && config && displayedData && dataSource.data) {
 			const fakeWidget = document.getElementById(el).firstChild
 			fakeWidget && document.getElementById(el).removeChild(fakeWidget)
-			typeof renderFunc === 'function' /* && Object.keys(config).length */ && renderFunc(dataSource, config, el)
+			let tablevar = typeof renderFunc === 'function' && await renderFunc(dataSource, config, el)
+			tablevar && setTable(tablevar)
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [JSON.stringify(config), JSON.stringify(dataSource), displayedData, renderFunc])
@@ -13,6 +19,7 @@ function WidgetView({ el, config, dataSource, displayedData, children, renderFun
 	return (
 		<>
 			{children}
+			{'columns' in config && <CsvIcon onClick={downloadCSV} />}
 			<div className="table-striped" style={{'width': '100%', 'height': '100%', 'overflowY': 'hidden'}} id={el} />
 		</>
 	)
