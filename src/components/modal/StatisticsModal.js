e6dd6bd97f186002bf37505a7e8fae39d15873cd
Spendil <spendil@mail.ru>
2022-05-23 09:50:58 +0300

    metrics info

--- src/components/modal/StatisticsModal.js
+++ src/components/modal/StatisticsModal.js
@@ -4,6 +4,16 @@ import { QueriesStore, UserStore } from '../../store/queriesStore'
 import { useInterval } from '../../utils/useInterval'
 import modalStore from '../../store/modalStore'
 import Loader from "react-loader-spinner"
+
+const METRICS_INFO = [
+	'Memory consumption by the query.',
+	'Number of bytes (the number of bytes before decompression) read from compressed sources (files, network).',
+	'RAM volume in bytes used to store a query result.',
+	`Total number of rows read from all tables and table functions participated in query. It includes usual subqueries, subqueries for IN and JOIN. For distributed queries read_rows includes the total number of rows read at all replicas. Each replica sends it's read_rows value, and the server-initiator of the query summarizes all received and local values. The cache volumes do not affect this value.`,
+	'Number of rows in a result of the SELECT query, or a number of rows in the INSERT query.',
+	'CPU time spent seen by OS. Does not include involuntary waits due to virtualization.',
+	'Number of SQL requests.'
+]
   
 const StatisticsModal = observer(function StatisticsModal({active}) {
 	const { currentQuery: { points, graphqlQueryID, graphqlRequested } } = QueriesStore
@@ -11,6 +21,7 @@ const StatisticsModal = observer(function StatisticsModal({active}) {
 	const { toggleModal, toggleStatisticsModal } = modalStore
 	const [metrics, setMetrics] = useState(undefined)
 	const [count, setCount] = useState(0)
+	const [metricNumber, setMetricNumber] = useState(null)
 
 	const getMetrics = async () => {
 		if (user.key) {
@@ -56,6 +67,7 @@ const StatisticsModal = observer(function StatisticsModal({active}) {
 		} else
 		if (metrics) {
 			modal = <div className={'modal__form '+(!active && 'modal__form_hide')}>
+						
 						<i className="handler handler__close fas fa-times" onClick={closeHandler} />
 						<table class="table table-bordered table-striped statisticsTable mt-4">
 							<thead>
@@ -69,7 +81,16 @@ const StatisticsModal = observer(function StatisticsModal({active}) {
 							<tbody>
 								{metrics.list.map(metric => {return (
 									<tr>
-										<th scope="row">{metric.name}</th>
+										<th scope="row">
+											<span 
+												className="metrics__helper"
+												onMouseEnter={e => setMetricNumber(e.target.parentNode.parentNode.rowIndex-1)}
+												onMouseLeave={()=>setMetricNumber(null)}
+											>
+												?
+											</span>
+											{metric.name}
+										</th>
 										<td style={{ "--part": `${Math.round(metric.value/metric.max*100)<20 ? 20 : Math.round(metric.value/metric.max*100)}%`}}>{(metric.cost/metric.price).toFixed(2)}</td>
 										<td>{metric.price.toFixed(2)}</td>
 										<td>{metric.cost.toFixed(2)}</td>
@@ -82,6 +103,7 @@ const StatisticsModal = observer(function StatisticsModal({active}) {
 							</tfoot>
 						</table>
 						<p>Query ID: {graphqlQueryID}</p>
+						{metricNumber !== null && <div className="metrics__info">{METRICS_INFO[metricNumber]}</div>}
 					</div>
 		} else {
 			modal = <div className={'modal__form '+(!active && 'modal__form_hide')}>
