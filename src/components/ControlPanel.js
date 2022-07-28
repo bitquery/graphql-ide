16347be31e836b402ac33ac2d51e2f30e25c44f0
Spendil <spendil@mail.ru>
2022-07-28 13:28:47 +0300

    menu active yu

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -3,7 +3,7 @@ import ProfileComponent from './ProfileComponent'
 import { QueriesStore } from '../store/queriesStore'
 import { observer } from 'mobx-react-lite'
 import useDebounce from '../utils/useDebounce'
-import { Link, useHistory } from 'react-router-dom'
+import { Link, useHistory, useLocation } from 'react-router-dom'
 import logo from '../assets/images/bitquery_logo_w.png'
 import { GalleryStore } from '../store/galleryStore'
 import { getSearchResults } from '../api/api'
@@ -13,19 +13,24 @@ const ControlPanel = observer(function ControlPanel() {
 	const { isMobile, setSearchValue } = QueriesStore
 	const { setCurrentTag } = GalleryStore
 	const [search, setSearch] = useState('')
+	const [active, setActive] = useState(1)
 	const searchValue = useDebounce(search, 500)
 	const history = useHistory()
 
 	useEffect(() => {
 		setSearchValue(searchValue)
 		searchValue && history.push('/graphql/explore')
+		if (searchValue) {
+			history.push('/graphql/explore')
+			setActive(2)
+		}
 	}, [searchValue])
 
 	const searchHandler = () => {
 		setSearch('')
 	}
 	return (
-		<div class="navbar navbar-expand-md navbar-light bg-white mb-2">
+		<div className="navbar navbar-expand-md navbar-light bg-white mb-2">
 			<a href="https://bitquery.io" className="navbar-brand topBar__logo">
 				<img
 					className="topBar__logo__img"
@@ -33,22 +38,22 @@ const ControlPanel = observer(function ControlPanel() {
 					alt="logo"
 				/>
 			</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
+			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon"></span>
 			</button>
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<form class="form-inline my-2 my-lg-0">
-					<input value={search} onChange={e=>setSearch(e.target.value)} ref={element=>(element||{}).onsearch=searchHandler} class="form-control form-control-sm mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+			<div className="collapse navbar-collapse" id="navbarSupportedContent">
+				<form className="form-inline my-2 my-lg-0">
+					<input value={search} onChange={e=>setSearch(e.target.value)} ref={element=>(element||{}).onsearch=searchHandler} className="form-control form-control-sm mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
 				</form>
-				<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-					<li class="nav-item active">
-						<Link class="nav-link text-secondary" to={"/graphql"}><i class="bi bi-play"></i>Develop</Link>
+				<ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+					<li className='nav-item'>
+						<Link className={`nav-link ${active === 1 ? 'text-secondary' : 'text-primary'}`} to={"/graphql"} onClick={() => setActive(1)} ><i className="bi bi-play"></i>Develop</Link>
 					</li>
-					<li class="nav-item">
-						<Link class="nav-link text-primary" to={"/graphql/explore"} ><i class="bi bi-terminal"></i>Explore</Link>
+					<li className='nav-item'>
+						<Link className={`nav-link ${active === 2 ? 'text-secondary' : 'text-primary'}`} to={"/graphql/explore"} onClick={() => setActive(2)} ><i className="bi bi-terminal"></i>Explore</Link>
 					</li>
-					<li class="nav-item">
-						<a class="nav-link text-primary" href="# " onClick={() => setCurrentTag('My queries')}><i class="bi bi-star"></i>My Queries</a>
+					<li className='nav-item'>
+						<Link className={`nav-link ${active === 3 ? 'text-secondary' : 'text-primary'}`} to={"/graphql/explore"} onClick={() => {setCurrentTag('My queries');setActive(3);}}><i className="bi bi-star"></i>My Queries</Link>
 					</li>
 				</ul>
 				<ProfileComponent />
