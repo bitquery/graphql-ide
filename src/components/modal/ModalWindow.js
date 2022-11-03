7d979d0747e6809af9317c83d58d295fe3ed14e4
Spendil <spendil@mail.ru>
2022-11-03 08:10:05 +0300

    remove modal windows

--- src/components/modal/ModalWindow.js
+++ src/components/modal/ModalWindow.js
@@ -1,34 +1,24 @@
 import React from 'react'
-import LoginForm from './LoginForm'
 import EditDialog from './EditDialog'
 import ApiKeyModal from './ApiKeyModal'
-import RegisterForm from './RegisterForm'
 import { observer } from 'mobx-react-lite'
 import modalStore from '../../store/modalStore'
 import StatisticsModal from './StatisticsModal'
-import ForgotPasswordForm from './ForgotPasswordForm'
-import ChangePasswordForm from './ChangePasswordForm'
 import DashbaordSettings from './DashbaordSettings'
 import { Modal } from 'react-bootstrap'
 
 const ModalWindow = observer(() => {
 	const { 
-		fade, registerIsOpen, loginIsOpen, modalIsOpen,
-		editDialogIsOpen, apiKeyIsOpen, statisticsModalIsOpen,
-		forgotPasswordIsOpen, changePasswordIsOpen, dashboardSettingsIsOpen,
-		toggleModal, toggleLogin, toggleRegister, toggleStatisticsModal,
-		toggleEditDialog, toggleApiKey,
-		toggleForgotPassword, toggleChangePassword, toggleDashboardSettings
+		fade, modalIsOpen, editDialogIsOpen, apiKeyIsOpen,
+		statisticsModalIsOpen, dashboardSettingsIsOpen,
+		toggleModal, toggleStatisticsModal, toggleEditDialog,
+		toggleApiKey, toggleDashboardSettings
 	} = modalStore
 	
 	const onRequestClose = () => {
 		toggleModal()
-		loginIsOpen && toggleLogin()
 		apiKeyIsOpen && toggleApiKey()
-		registerIsOpen && toggleRegister()
 		editDialogIsOpen && toggleEditDialog()
-		forgotPasswordIsOpen && toggleForgotPassword()
-		changePasswordIsOpen && toggleChangePassword()
 		statisticsModalIsOpen && toggleStatisticsModal()
 		dashboardSettingsIsOpen && toggleDashboardSettings()
 	}
@@ -41,13 +31,9 @@ const ModalWindow = observer(() => {
 			size={statisticsModalIsOpen ? 'lg' : 'md'}
 			onHide={fade ? null : onRequestClose}
 		>
-			<LoginForm active={loginIsOpen} />
 			<ApiKeyModal active={apiKeyIsOpen} />
 			<EditDialog active={editDialogIsOpen} />
-			<RegisterForm active={registerIsOpen} />
 			<StatisticsModal active={statisticsModalIsOpen} />
-			<ChangePasswordForm active={changePasswordIsOpen} />
-			<ForgotPasswordForm active={forgotPasswordIsOpen} />
 			<DashbaordSettings active={dashboardSettingsIsOpen} />
 		</Modal>
     )
