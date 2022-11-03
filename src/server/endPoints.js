8080e1ef00b84ec89910aef9ed67b998c9c63253
Spendil <spendil@mail.ru>
2022-11-03 08:02:29 +0300

    remove endpoints

--- src/server/endPoints.js
+++ src/server/endPoints.js
@@ -1,9 +1,6 @@
-const transporter = require('./mailer')
 const crypto = require('crypto')
-const bcrypt = require('bcrypt')
 const fs = require('fs')
 const path = require('path')
-const axios = require('axios')
 const sdk = require('postman-collection')
 const codegen = require('postman-code-generators')
 
@@ -326,31 +323,6 @@ module.exports = function(app, passport, db, redisClient) {
 			}
 		}
 	}
-	const sendActivationLink = (userID, userEmail, req) => {
-		let code = crypto.randomBytes(60).toString('hex')
-		db.query('select * from activations where user_id=?', [userID], (err, result) => {
-			if (err) console.log(err)
-			console.log('result', result)
-			if (result.length) {
-				db.query('update activations set code=? where user_id=?', [code, userID], (err, _) => {
-					if (err) console.log(err)
-				})
-			} else {
-				db.query('INSERT INTO activations SET ?', {user_id: userID, code: code}, (err, res) => {
-					if (err) console.log(err)
-					console.log(res)
-				})
-			}
-		})
-		let message = {
-			from : process.env.DEFAULT_EMAIL,
-			to: userEmail,
-			subject: 'Account activations',
-			text: 'Plaintext version of the message',
-			html: `<p>To activate your account follow this <a href="${process.env.IDE_URL}/api/activate?code=${code}">link</a> </p>`
-		}
-		transporter.sendMail(message)
-	}
 
 	app.get('/api/getw/:url', (req, response) => {
 		db.query(`SELECT rd.id, rd.account_id, null as query, null as variables, 
@@ -385,8 +357,6 @@ module.exports = function(app, passport, db, redisClient) {
 	})
 
 	app.post('/api/savedashboard', (req, response) => {
-		/* console.log(req.body)
-		response.send(200) */
 		const params = {
 			layout: JSON.stringify(req.body.layout),
 			name: req.body.name,
@@ -500,111 +470,7 @@ module.exports = function(app, passport, db, redisClient) {
 			response.send({msg: 'Dashboard saved!', id: res.insertId})
 		})	
 	}) 
-
-	app.post('/api/regenerate', (req, res) => {
-		db.query('update api_keys set active=false, updated_at=CURRENT_TIMESTAMP where user_id=? and active=true',
-		[req.session.passport.user], (error, _) => {
-			if (error) console.log(error)
-			db.query('insert into api_keys SET ?', {
-				user_id: req.session.passport.user,
-				key: req.body.key,
-				active: true
-			}, (err, _) => {
-				if (err) console.log(err)
-				res.send(200)
-			})
-		})
-	})
-	app.post('/api/login', (req, res, next) => {
-		passport.authenticate('local-login', (err, user, info) => {
-			if (err) {
-				return next(err)
-			}
-			if (!user) {
-				return res.status(400).send("Cannot log in")
-			} else if (!user[0].active) {
-				return res.status(400).send('Account not activated! If You do not recieve mail, can resend it in login window')
-			} else {
-				req.login(user, (err) => {
-					console.log(user)
-					res.send("Logged in")
-				}) 
-			}
-		})(req, res, next)
-	})
-
-	function verifyUser(value) {
-		const params = new URLSearchParams()
-		params.append('secret', process.env.CAPTCHA)
-		params.append('response', value)
-		const config = {
-			headers: {
-			  'Content-Type': 'application/x-www-form-urlencoded'
-			}
-		}
-		return axios.post('https://www.google.com/recaptcha/api/siteverify', params, config)
-	}
-
-	app.post('/api/signup', (req, res, next) => {
-		verifyUser(req.body.captcha).then(result => {
-			if (result.data.success) {
-				passport.authenticate('local-signup', (err, user, message) => {
-					if(err) return next(err)
-					if (!user) {
-						return res.status(400).send(message?.message || "user already exist")
-					}
-					db.query('UPDATE accounts SET name=?, company_name=? WHERE email=?', [
-						req.body.accountName,
-						req.body.companyName,
-						req.body.email
-					], (err, _) => {
-						if (err) console.log(err)
-						axios.post(`https://api.hubapi.com/contacts/v1/contact/?hapikey=${process.env.HUBSPOT}`,
-							{
-								properties: [
-									{
-										"property": "email",
-										"value": req.body.email
-									},
-									{
-										"property": "firstname",
-										"value": req.body.accountName
-									},
-									{
-										"property": "company",
-										"value": req.body.companyName
-									},
-									{
-										"property": "channel",
-										"value": "IDE"
-									}
-								]
-							},
-						).catch(error => {
-							console.log(error.response.data)
-						})
-						sendActivationLink(user[0].id, user[0].email, req)
-						res.send('Activation link sent. Check your email for further instructions!')
-					})
-				})(req, res, next)
-			} else {
-				res.status(400).send('bad')
-			}
-		})
-	})
-	app.post('/api/resendactivation', (req, res) => {
-		let userEmail = req.body.email
-		db.query('select id from accounts where email = ?', [req.body.email], (err, result) => {
-			if (err) console.log(err)
-			if (result[0].id) {
-				sendActivationLink(result[0].id, userEmail, req)
-				res.send('Activation link sent. Check your email for further instructions!')
-			} else {
-				res.send('There is no such user with this email')
-			}
-		})
-	})
-
+	
 	app.post('/api/addquery', (req, res) => {
 		if (req.session.passport.user) {
 			let query = req.body.params
@@ -746,48 +612,6 @@ module.exports = function(app, passport, db, redisClient) {
 				res.send(queries)
 		})
 	})
-	
-	app.get('/api/activate', (req, res) => {
-		db.query(`select * from activations where code = ?`, [req.query.code], (err, result) => {
-			if (err) console.log(err)
-			if (result.length) {
-				const user_id = result[0].user_id
-				db.query(`update accounts set active = true where id = ?`, [user_id], (err, result) => {
-					if (err) console.log(err)
-					console.log('account activated', result)
-					req.session.active = true
-					db.query('SELECT * from billing_periods WHERE user_id = ?', [user_id], (err, billing_period) => {
-						if (err) console.log(err)
-						if (!billing_period.length) {
-							const billingPeriods = {}
-							const now = new Date()
-							billingPeriods.user_id = user_id
-							billingPeriods.started_at = new Date ( now.setUTCHours(0, 0, 0, 0) ).toISOString().replace('T', ' ').replace('Z', '')
-							now.setDate( now.getDate() + 30 )
-							now.setUTCHours(23, 59, 59)
-							const ended_at = now.toISOString()
-							billingPeriods.ended_at = ended_at.replace('T', ' ').replace('Z', '')
-							billingPeriods.created_at = new Date().toISOString().replace('T', ' ').replace('Z', '')
-							billingPeriods.updated_at = new Date().toISOString().replace('T', ' ').replace('Z', '')
-							billingPeriods.day_of_month = new Date().getDate()
-							db.query('INSERT INTO billing_periods SET ?', {...billingPeriods}, (err, _) => {
-								if (err) console.log(err)
-								process.env.NODE_ENV==='production'
-									? res.redirect(`${process.env.IDE_URL}`)
-									: res.redirect(`http://localhost:3005`)
-							})
-						} else {
-							process.env.NODE_ENV==='production'
-										? res.redirect(`${process.env.IDE_URL}`)
-										: res.redirect(`http://localhost:3005`)
-						}
-					})
-				})
-			} else {
-				res.send('Something went wrong...')
-			}
-		})
-	})
 
 	app.get('/api/js', (req, res) => {
 		const filePath = path.resolve(__dirname, req.query.source)
@@ -798,78 +622,4 @@ module.exports = function(app, passport, db, redisClient) {
 		})
 	})
 
-	app.post('/api/forgot', (req, res) => {
-		let token = crypto.randomBytes(20).toString('hex')
-		db.query(`update accounts set reset_token = ? where email = ?`, [token, req.body.email], (err, result) => {
-			if (err) console.log(err)
-			if (result.affectedRows) {
-				let message = {
-					from : process.env.DEFAULT_EMAIL,
-					to: req.body.email,
-					subject: 'Account password reset',
-					text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-						'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-						process.env.IDE_URL + '/api/reset/' + token + '\n\n' +
-						'If you did not request this, please ignore this email and your password will remain unchanged.\n',
-				}
-				transporter.sendMail(message)
-				res.send('An e-mail has been sent to ' + req.body.email + ' with further instructions.')
-			} else {
-				res.send('There is no such account with this email')
-			}
-		})
-	})
-
-	app.get('/api/reset/:token', (req, res) => {
-		db.query(`select id from accounts where reset_token = ?`, [req.params.token], (err, result) => {
-			if (err) console.log(err)
-			if (result.length) {
-				process.env.NODE_ENV==='production'
-					? res.redirect(`${process.env.IDE_URL}/reset/${req.params.token}`)
-					: res.redirect(`http://localhost:3000/reset/${req.params.token}`)
-			} else {
-				res.send('Something went wrong')
-			}
-		})
-	})
-
-	app.post('/api/reset', (req, res) => {
-		bcrypt.hash(req.body.password, 5, (err, hash) => {
-			if (err) console.log(err)
-			if (req.body.token) {
-				db.query(`update accounts set encrypted_credentials = ? where reset_token = ?`, [hash, req.body.token], (err, result) => {
-					if (err) console.log(err)
-					console.log(result)
-					res.send('Password changed!')
-				})
-			} else if (req.session.passport.user) {
-				db.query(`update accounts set encrypted_credentials = ? where id = ?`, [hash, req.session.passport.user], (err, result) => {
-					if (err) console.log(err)
-					res.send('Password changed!')
-				})
-			} else { res.status(400).send('Token expired!') }
-		})
-	})
-
-	app.post('/api/changepassword', (req, res) => {
-		db.query(`select encrypted_credentials from accounts where id = ?`, [req.session.passport.user], (err, pass) => {
-			bcrypt.compare(req.body.oldPwd, pass[0][Object.keys(pass[0])[0]], (err, result) => {
-				if (err) console.log(err)
-				if (result) {
-					bcrypt.hash(req.body.newPwd, 5, (err, hash) => {
-						if (err) console.log(err)
-						db.query(`update accounts set encrypted_credentials = ? where id = ?`, [hash, req.session.passport.user], (err, result) => {
-							if (err) console.log(err)
-							console.log(result)
-							res.send('Password changed!')
-						})
-					})
-				} else {
-					res.status(400).send('Wrong password!')
-				}
-			})
-		})
-		
-	})
-	
 }
