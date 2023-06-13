909bd9038fd00aeab18f367c012b6327d96bf62c
Spendil <spendil@mail.ru>
2023-06-13 19:21:47 +0700

    Revert "revert to current main"
    This reverts commit 4fc34f4e6e5595c07e6b9a4f99c86b1c3fa094d4.

--- src/components/bitqueditor/components/WidgetView.js
+++ src/components/bitqueditor/components/WidgetView.js
@@ -1,26 +1,38 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import CsvIcon from '../../icons/CsvIcon'
+import { QueriesStore } from '../../../store/queriesStore'
 
-function WidgetView({ el, config, dataSource, displayedData, children, renderFunc }) {
+function WidgetView({ el, config, dataSource, displayedData, children, renderFunc, widget, widgetInstance, setWidgetInstance }) {
+	const { currentQuery } = QueriesStore
+	const ref = useRef(null)
 	const [table, setTable] = useState(null)
 	const downloadCSV = () => {
 		table.download('csv', 'data.csv')
 	}
 	useEffect(async () => {
-		if (dataSource && config && displayedData && dataSource.data) {
-			const fakeWidget = document.getElementById(el).firstChild
-			fakeWidget && document.getElementById(el).removeChild(fakeWidget)
-			let tablevar = typeof renderFunc === 'function' && await renderFunc(dataSource, config, el)
-			tablevar && setTable(tablevar)
+		if (widget) {
+			/* while(ref.current.firstChild) {
+				ref.current.removeChild(ref.current.firstChild)
+			} */
+			// const fakeWidget = document.getElementById(el).firstChild
+			// fakeWidget && document.getElementById(el).removeChild(fakeWidget)
+			// let tablevar = typeof renderFunc === 'function' && await renderFunc(dataSource, config, el)
+			// tablevar && setTable(tablevar)
+			const Widget = eval(`(${widget})`)
+			const widgetInstance = new Widget(ref.current, currentQuery.query)
+			setWidgetInstance(widgetInstance)
+			if (dataSource?.data) {
+				widgetInstance.onData(dataSource.data, true)
+			}
 		}
 		// eslint-disable-next-line 
-	}, [JSON.stringify(config), JSON.stringify(dataSource), displayedData, renderFunc])
-	if (!dataSource || !displayedData) return (<div></div>)
+	}, [JSON.stringify(config), JSON.stringify(dataSource.data), widget, currentQuery.widget_id])
+
 	return (
 		<>
 			{children}
-			{config && 'columns' in config && <CsvIcon onClick={downloadCSV} />}
-			<div className="table-striped" style={{'width': '100%', 'height': '100%', 'overflowY': 'hidden'}} id={el} />
+			{/* NEED CONDITION FOR CSV DOWNLOAD BUTTON {config && 'columns' in config && <CsvIcon onClick={downloadCSV} />} */}
+			<div ref={ref} className="table-striped" style={{'width': '100%', 'height': '100%', 'overflowY': 'scroll'}} id={el} />
 		</>
 	)
 }
