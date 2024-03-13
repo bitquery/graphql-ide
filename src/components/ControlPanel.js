bb95d1b64902cd7802d06a32f9936eab5f718d9f
Nadia <nadia.olerinskaia@gmail.com>
2024-03-13 10:09:39 +0200

    BIT-5886 fixed ide layout

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,137 +1,192 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 import ProfileComponent from './ProfileComponent'
-import { QueriesStore, UserStore } from '../store/queriesStore'
-import { observer } from 'mobx-react-lite'
+import {QueriesStore, UserStore} from '../store/queriesStore'
+import {observer} from 'mobx-react-lite'
 import useDebounce from '../utils/useDebounce'
-import { Link, useHistory, useLocation } from 'react-router-dom'
+import {Link, useHistory, useLocation} from 'react-router-dom'
 import logo from '../assets/images/bitquery_logo_w.png'
-import { GalleryStore } from '../store/galleryStore'
-import { useQuery } from '../utils/useQuery'
+import {GalleryStore} from '../store/galleryStore'
+import {useQuery} from '../utils/useQuery'
 
 const ControlPanel = observer(function ControlPanel() {
-	const { user } = UserStore
-	const { currentQuery} = QueriesStore
-	const { currentTag } = GalleryStore
-	const [search, setSearch] = useState('')
-	const [active, setActive] = useState(1)
-	const searchValue = useDebounce(search, 500)
-	const history = useHistory()
-	const location = useLocation()
-	const query = useQuery()
-	
-	useEffect(() => {
-		query.set('search', searchValue)
-		searchValue && history.push({
-			pathname: '/explore/All%20queries',
-			search: `${searchValue ? '?search=' : ''}${searchValue}`
-		})
-	}, [searchValue])
+    const {user} = UserStore
+    const {currentQuery} = QueriesStore
+    const {currentTag} = GalleryStore
+    const [search, setSearch] = useState('')
+    const [active, setActive] = useState(1)
+    const searchValue = useDebounce(search, 500)
+    const history = useHistory()
+    const location = useLocation()
+    const query = useQuery()
+    const menuItems = {
+        queries: { name: 'Queries', icon: <i className="bi bi-terminal mr-2"></i> },
+        explore: { name: 'Explore', icon: <i className="bi bi-terminal mr-2"></i> },
+        myqueries: { name: 'My Queries', icon: <i className="bi bi-star mr-2"></i> },
+        teamqueries: { name: 'Team Queries', icon: <i className="bi bi-people mr-2"></i> }
+    }
+    const [queriesMenuTitle, setQueriesMenuTitle] = useState(menuItems.queries)
+    useEffect(() => {
+        query.set('search', searchValue)
+        searchValue && history.push({
+            pathname: '/explore/All%20queries',
+            search: `${searchValue ? '?search=' : ''}${searchValue}`
+        })
+    }, [searchValue])
 
-	useEffect(() => {
-		currentTag !== 'All queries' && setSearch('')
-	}, [currentTag])
-	
-	useEffect(() => {
-		const queryListType = location.pathname.match(/[a-zA-Z]+/gm)?.[0]
-		if (queryListType === 'explore') {
-			setActive(2)
-		} else if (queryListType === 'myqueries') {
-			setActive(3)
-		} else if (queryListType === 'team') {
-			setActive(4)
-		} else {
-			setActive(1)
-		}
-	}, [location.pathname])
+    useEffect(() => {
+        currentTag !== 'All queries' && setSearch('')
+    }, [currentTag])
 
-	const searchHandler = (e) => {
-		e.preventDefault()
-		if (e.which = 13) {
-			return false
-		} else {
-			setSearch('')
-		}
-	}
+    useEffect(() => {
+        const queryListType = location.pathname.match(/[a-zA-Z]+/gm)?.[0]
+        if (queryListType === 'explore') {
+            setActive(3)
+        } else if (queryListType === 'myqueries') {
+            setActive(4)
+        } else if (queryListType === 'team') {
+            setActive(5)
+        } else {
+            setActive(1)
+        }
+    }, [location.pathname])
 
-	const onClick = title => {
-		setSearch('')
-		document.title = title
-	}
+    const searchHandler = (e) => {
+        e.preventDefault()
+        if (e.which = 13) {
+            return false
+        } else {
+            setSearch('')
+        }
+    }
 
-	return (
-		<div className="navbar navbar-expand-lg navbar-light bg-white mb-2">
-			<a href="#tabs" className="skip-to-main-content" >Skip to main content</a>
-			<a href='/' className="navbar-brand topBar__logo">
-				<img
-					className="topBar__logo__img"
-					src={logo}
-					alt="Bitquery"
-				/>
-			</a>
-			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span className="navbar-toggler-icon"></span>
-			</button>
-			<div className="collapse navbar-collapse" id="navbarSupportedContent" >
-				<form className="form-inline my-2 my-lg-0" onSubmit={searchHandler} action="search">
-					<input value={search} onChange={e=>setSearch(e.target.value)} className="form-control form-control-sm mr-sm-2" type="search" placeholder="Search Queries" aria-label="Search" />
-				</form>
-				<ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-					<li className='nav-item' tabIndex='-1'>
-						<Link className={`nav-link ${active === 1 ? 'nav-active' : 'navigation-link-color'}`} to='/' onClick={() => onClick(currentQuery.name || 'New Query')}  ><i className="bi bi-play mr-2"></i>Develop</Link>
-					</li>
-					<li className='nav-item' tabIndex='-1'>
-						<Link className={`nav-link ${active === 2 ? 'nav-active' : 'navigation-link-color'}`} to={`/explore/All%20queries`} onClick={() => onClick('Explore')} ><i className="bi bi-terminal mr-2"></i>Explore</Link>
-					</li>
-					{user?.id && <li className='nav-item' tabIndex='-1'>
-						<Link className={`nav-link ${active === 3 ? 'nav-active' : 'navigation-link-color'}`} to={`/myqueries/All%20queries`} onClick={() => onClick('My Queries')}><i className="bi bi-star mr-2"></i>My Queries</Link>
-					</li>}
-					{(user?.children_count || user?.ancestry) && <li className='nav-item' tabIndex='-1'>
-						<Link className={`nav-link ${active === 4 ? 'nav-active' : 'navigation-link-color'}`} to={`/team/All%20queries`} onClick={() => onClick('Team')}><i className="bi bi-people mr-2"></i>Team</Link>
-					</li>}
-					<li className="nav-item d-lg-none" tabIndex='-1'>
-						<a className="nav-link navigation-link-color" href="https://discord.gg/EEBVTQnb2E"><i className="bi bi-discord">Discord</i></a>
-					</li>
-					<li className="nav-item d-lg-none" tabIndex='-1'>
-						<a className="nav-link navigation-link-color" href="https://t.me/bloxy_info/"><i className="bi bi-telegram mr-2"></i>Telegram</a>
-					</li>
-					<li className="nav-item d-lg-none" tabIndex='-1'>
-						<a className="nav-link navigation-link-color" href="https://streaming.bitquery.io/tutorial/">Streaming API (V2  API Docs)</a>
-					</li>
-					<li className="nav-item d-lg-none" tabIndex='-1'>
-						<a className="nav-link navigation-link-color" href="https://community.bitquery.io/">Forum</a>
-					</li>
-					<li className="nav-item d-lg-none" tabIndex='-1'>
-						<a className="nav-link navigation-link-color" href="https://community.bitquery.io/t/how-to-get-started-with-bitquerys-blockchain-graphql-apis/13">Getting started</a>
-					</li>
-					<li className="nav-item d-lg-none" tabIndex='-1'>
-						<a className="nav-link navigation-link-color" href="https://angel.co/company/bitquery/jobs">We are hiring!</a>
-					</li>
-					<li className="nav-item d-lg-none" tabIndex='-1'>
-						<a className="nav-link navigation-link-color" href="https://account.bitquery.io/user/billing">Upgrade</a>
-					</li>
-					{!user?.id && <li className="nav-item d-lg-none">
-						<a className="nav-link navigation-link-color" href={`${user?.graphql_admin_url}/auth/login?redirect_to=${window.location.href}`}>Login</a>
-					</li>}
-					{ user?.role === 'admin' && <li className="nav-item d-lg-none">
-						<a className="nav-link navigation-link-color" href={`${user?.graphql_admin_url}/admin/accounts`}>Admin</a>
-					</li>}
-					{user?.id && <li className="nav-item d-lg-none">
-						<a className="nav-link navigation-link-color" href={`${user?.graphql_admin_url}/team/members/new`}>Invite team member</a>
-					</li>}
-					{user?.id && <li className="nav-item d-lg-none">
-						<a className="nav-link navigation-link-color" href={`${user?.graphql_admin_url}/user/account`}>Account</a>
-					</li>}
-					{user?.id && <li className="nav-item d-lg-none">
-						<a className="nav-link navigation-link-color" href={`${user?.graphql_admin_url}/user/api_key`}>API key</a>
-					</li>}
-					{user?.id && <li className="nav-item d-lg-none">
-						<a className="nav-link navigation-link-color" href={`${user?.graphql_admin_url}/auth/logout`}>Logout</a>
-					</li>}
-				</ul>
-				<ProfileComponent />
-			</div>
-		</div>
-	)
+    const onClick = title => {
+        setSearch('')
+        document.title = title
+    }
+	const onClickWithStartTitle = title => {
+        setSearch('')
+        document.title = title
+		setQueriesMenuTitle(menuItems.queries)
+
+    }
+    const onClickWithTitle = (itemKey) => {
+        setSearch('')
+        document.title = menuItems[itemKey].name
+        setQueriesMenuTitle(menuItems[itemKey])
+    }
+
+    return (
+        <div className="navbar navbar-expand-lg navbar-light bg-white mb-2">
+            <a href="#tabs" className="skip-to-main-content">Skip to main content</a>
+            <a href='/' className="navbar-brand topBar__logo">
+                <img
+                    className="topBar__logo__img"
+                    src={logo}
+                    alt="Bitquery"
+                />
+            </a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <form className="form-inline my-2 my-lg-0" onSubmit={searchHandler} action="search">
+                    <input value={search} onChange={e => setSearch(e.target.value)}
+                           className="form-control form-control-sm mr-sm-2" type="search" placeholder="Search Queries"
+                           aria-label="Search"/>
+                </form>
+                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li className='nav-item' tabIndex='-1'>
+                        <Link className={`nav-link ${active === 1 ? 'nav-active' : 'navigation-link-color'}`} to='/'
+                              onClick={() => onClickWithStartTitle(currentQuery.name || 'New Query')}><i
+                            className="bi bi-play mr-2"></i>Develop</Link>
+                    </li>
+                    {/*<li className='nav-item' tabIndex='-1'>*/}
+                    {/*	<Link className={`nav-link ${active === 2 ? 'nav-active' : 'navigation-link-color'}`} to={`/explore/All%20queries`} onClick={() => onClick('Explore')} ><i className="bi bi-terminal mr-2"></i>Explore</Link>*/}
+                    {/*</li>*/}
+                    {/*{user?.id && <li className='nav-item' tabIndex='-1'>*/}
+                    {/*	<Link className={`nav-link ${active === 3 ? 'nav-active' : 'navigation-link-color'}`} to={`/myqueries/All%20queries`} onClick={() => onClick('My Queries')}><i className="bi bi-star mr-2"></i>My Queries</Link>*/}
+                    {/*</li>}*/}
+                    {/*{(user?.children_count || user?.ancestry) && <li className='nav-item' tabIndex='-1'>*/}
+                    {/*	<Link className={`nav-link ${active === 4 ? 'nav-active' : 'navigation-link-color'}`} to={`/team/All%20queries`} onClick={() => onClick('Team')}><i className="bi bi-people mr-2"></i>Team</Link>*/}
+                    {/*</li>}*/}
+                    <li className="nav-item dropdown"  tabIndex='-1'>
+                        <a className={`nav-link ${(active === 2 || active === 3 ||active === 4 || active === 5 )? 'nav-active' : 'navigation-link-color'} dropdown-toggle`}   href="#"
+                           id="navbarDropdownMenuLink" role="button"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {queriesMenuTitle.icon}
+                            {queriesMenuTitle.name}
+                        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink"  tabIndex='-1'>
+                            <Link className={`nav-link ${active === 3 ? 'nav-active' : 'navigation-link-color'} dropdown-item`} to={`/explore/All%20queries`}
+                                  onClick={() => onClickWithTitle('explore')}>{menuItems.explore.icon}Explore</Link>
+                            {user?.id &&
+                                <Link className={`nav-link ${active === 4 ? 'nav-active' : 'navigation-link-color'} dropdown-item`} to={`/myqueries/All%20queries`}
+                                      onClick={() => onClickWithTitle('myqueries')}>{menuItems.myqueries.icon}My
+                                    Queries</Link>}
+                            {(user?.children_count || user?.ancestry) &&
+                                <Link   className={`nav-link ${active === 5 ? 'nav-active' : 'navigation-link-color'} dropdown-item`} to={`/team/All%20queries`}
+                                      onClick={() => onClickWithTitle('teamqueries')}>{menuItems.teamqueries.icon}Team
+                                    Queries</Link>}
+                        </div>
+                    </li>
+                    <li className="nav-item d-lg-none" tabIndex='-1'>
+                        <a className="nav-link navigation-link-color" href="https://discord.gg/EEBVTQnb2E"><i
+                            className="bi bi-discord mr-2"></i>Discord</a>
+                    </li>
+                    <li className="nav-item d-lg-none" tabIndex='-1'>
+                        <a className="nav-link navigation-link-color" href="https://t.me/bloxy_info/"><i
+                            className="bi bi-telegram mr-2"></i>Telegram</a>
+                    </li>
+                    <li className="nav-item d-lg-none" tabIndex='-1'>
+                        <a className="nav-link navigation-link-color" href="https://streaming.bitquery.io/tutorial/">Documentation</a>
+                    </li>
+                    <li className="nav-item d-lg-none" tabIndex='-1'>
+                        <a className="nav-link navigation-link-color" href="https://community.bitquery.io/">Forum</a>
+                    </li>
+                    <li className="nav-item d-lg-none" tabIndex='-1'>
+                        <a className="nav-link navigation-link-color"
+                           href="https://community.bitquery.io/t/how-to-get-started-with-bitquerys-blockchain-graphql-apis/13">Getting
+                            started</a>
+                    </li>
+                    <li className="nav-item d-lg-none" tabIndex='-1'>
+                        <a className="nav-link navigation-link-color" href="https://angel.co/company/bitquery/jobs">We
+                            are hiring!</a>
+                    </li>
+                    <li className="nav-item d-lg-none" tabIndex='-1'>
+                        <a className="nav-link navigation-link-color"
+                           href="https://account.bitquery.io/user/billing">Upgrade</a>
+                    </li>
+                    {!user?.id && <li className="nav-item d-lg-none">
+                        <a className="nav-link navigation-link-color"
+                           href={`${user?.graphql_admin_url}/auth/login?redirect_to=${window.location.href}`}>Login</a>
+                    </li>}
+                    {user?.role === 'admin' && <li className="nav-item d-lg-none">
+                        <a className="nav-link navigation-link-color"
+                           href={`${user?.graphql_admin_url}/admin/accounts`}>Admin</a>
+                    </li>}
+                    {user?.id && <li className="nav-item d-lg-none">
+                        <a className="nav-link navigation-link-color"
+                           href={`${user?.graphql_admin_url}/team/members/new`}>Invite team member</a>
+                    </li>}
+                    {user?.id && <li className="nav-item d-lg-none">
+                        <a className="nav-link navigation-link-color"
+                           href={`${user?.graphql_admin_url}/user/account`}>Account</a>
+                    </li>}
+                    {user?.id && <li className="nav-item d-lg-none">
+                        <a className="nav-link navigation-link-color" href={`${user?.graphql_admin_url}/user/api_key`}>API
+                            key</a>
+                    </li>}
+                    {user?.id && <li className="nav-item d-lg-none">
+                        <a className="nav-link navigation-link-color"
+                           href={`${user?.graphql_admin_url}/auth/logout`}>Logout</a>
+                    </li>}
+                </ul>
+                <ProfileComponent/>
+            </div>
+        </div>
+    )
 })
 
 export default ControlPanel
--- src/components/ProfileComponent.js
+++ src/components/ProfileComponent.js
@@ -1,53 +1,75 @@
-import React, { useEffect } from 'react'
-import { observer } from 'mobx-react-lite'
-import { UserStore } from '../store/queriesStore'
-import UserIcon from './icons/UserIcon'
-import { Dropdown } from 'react-bootstrap'
+import React, { useEffect } from 'react';
+import { observer } from 'mobx-react-lite';
+import { UserStore } from '../store/queriesStore';
+import UserIcon from './icons/UserIcon';
+import { Dropdown } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
 const Profile = observer(() => {
+    const { getUser, user } = UserStore;
 
-	const { getUser, user } = UserStore
+    useEffect(() => {
+        getUser();
+        // eslint-disable-next-line
+    }, [])
 
-	useEffect(() => {
-		getUser()
-		// eslint-disable-next-line 
-	}, [])
+    return !user?.id ? (
+        <div className="flex profile__menu d-none d-lg-block" >
+            <a className="profile__email" href={`${user?.graphql_admin_url}/auth/login?redirect_to=${window.location.href}`}>
+                Login
+                <UserIcon />
+            </a>
+        </div>
+    ) : (
+        <div className="d-none d-lg-flex align-items-center justify-content-end">
+            <a className='link__hire navigation-link-color' target='_blank'
+               href='https://streaming.bitquery.io/tutorial/'>Docs</a>
+            <a className='link__hire navigation-link-color' target='_blank'
+               href='https://account.bitquery.io/user/billing'>Upgrade</a>
 
-	return !user?.id ? (
-		<div className="flex profile__menu d-none d-lg-block" >
-			<a className="profile__email" href={`${user?.graphql_admin_url}/auth/login?redirect_to=${window.location.href}`}>
-				Login
-				<UserIcon />
-			</a>
-		</div>
-	) : <Dropdown className={'d-none d-lg-block'}>
-			<a aria-label='discord' className='link__hire navigation-link-color' target='_blank' href='https://discord.gg/EEBVTQnb2E'>
-				<i className="bi bi-discord"></i>
-			</a>
-			<a aria-label='telegram' className='link__hire navigation-link-color' target='_blank' href='https://t.me/bloxy_info/'>
-				<i className="bi bi-telegram"></i>
-			</a>
-			<a className='link__hire navigation-link-color' target='_blank' href='https://streaming.bitquery.io/tutorial/'>Streaming API (V2  API Docs)</a>
-			<a className='link__hire navigation-link-color' target='_blank' href='https://community.bitquery.io/'>Forum</a>
-			<a className='link__hire navigation-link-color' target='_blank' href='https://community.bitquery.io/t/how-to-get-started-with-bitquerys-blockchain-graphql-apis/13'>Getting started</a>
-			<a className='link__hire navigation-link-color' target='_blank' href='https://angel.co/company/bitquery/jobs'>We are hiring!</a>
-			<a className='link__hire navigation-link-color' target='_blank' href='https://account.bitquery.io/user/billing'>Upgrade</a>
-			<Dropdown.Toggle className="cursor-pointer dropdown-toggler" as={'a'} role="button" tabIndex="0" aria-label="account menu">
-				<i className="bi bi-person"></i>
-			</Dropdown.Toggle>
-		
-			<Dropdown.Menu className="dropdown-menu dropdown-menu-right">
-				<Dropdown.Header>{user.email}</Dropdown.Header>
-				<Dropdown.Divider/>
-				{ user.role === 'admin' &&  <Dropdown.Item href={`${user?.graphql_admin_url}/admin/accounts`}>Admin</Dropdown.Item>}
-				{ user.role === 'admin' && <Dropdown.Divider/> }
-				<Dropdown.Item href={`${user?.graphql_admin_url}/team/members/new`}>Invite team member</Dropdown.Item>
-				<Dropdown.Divider/>
-				<Dropdown.Item href={`${user?.graphql_admin_url}/user/account`}>Account</Dropdown.Item>
-				<Dropdown.Item href={`${user?.graphql_admin_url}/user/api_key`}>API key</Dropdown.Item>
-				<Dropdown.Item href={`${user?.graphql_admin_url}/auth/logout`}>Logout</Dropdown.Item>
-			</Dropdown.Menu>
-		</Dropdown>
+            <Dropdown>
+                <Dropdown.Toggle variant="" className="link__hire navigation-link-color">
+                    Contacts us
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <Dropdown.Item className="link__hire navigation-link-color" href='https://discord.gg/EEBVTQnb2E' target='_blank'>
+                        Discord
+                    </Dropdown.Item>
+                    <Dropdown.Item className="link__hire navigation-link-color"  href='https://t.me/bloxy_info/' target='_blank'>
+                        Telegram
+                    </Dropdown.Item>
+                    <Dropdown.Item className="link__hire navigation-link-color"  href='https://community.bitquery.io/' target='_blank'>
+                        Forum
+                    </Dropdown.Item>
+                    <Dropdown.Item className="link__hire navigation-link-color" href='https://community.bitquery.io/t/how-to-get-started-with-bitquerys-blockchain-graphql-apis/13' target='_blank'>
+                        Getting started
+                    </Dropdown.Item>
+                    <Dropdown.Item className="link__hire navigation-link-color"  href='https://angel.co/company/bitquery/jobs' target='_blank'>
+                        We are hiring!
+                    </Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+
+            <Dropdown>
+                <Dropdown.Toggle variant="" className="cursor-pointer dropdown-toggler" as={'a'} role="button" tabIndex="0" aria-label="account menu">
+                    <i className="bi bi-person"></i>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu className="dropdown-menu dropdown-menu-right">
+                    <Dropdown.Header className="nav-active">{user.email}</Dropdown.Header>
+                    <Dropdown.Divider/>
+                    {user.role === 'admin' &&
+                        <Dropdown.Item className="link__hire navigation-link-color"  href={`${user?.graphql_admin_url}/admin/accounts`}>Admin</Dropdown.Item>}
+                    {user.role === 'admin' && <Dropdown.Divider/>}
+                    <Dropdown.Item className="link__hire navigation-link-color"  href={`${user?.graphql_admin_url}/team/members/new`}>Invite team member</Dropdown.Item>
+                    <Dropdown.Divider/>
+                    <Dropdown.Item className="link__hire navigation-link-color"  href={`${user?.graphql_admin_url}/user/account`}>Account</Dropdown.Item>
+                    <Dropdown.Item className="link__hire navigation-link-color"  href={`${user?.graphql_admin_url}/user/api_key`}>API key</Dropdown.Item>
+                    <Dropdown.Item className="link__hire navigation-link-color"  href={`${user?.graphql_admin_url}/auth/logout`}>Logout</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+        </div>
+    );
 })
 
-export default Profile
+export default Profile;
